10285bc56c1a0535d9ffce39a07d245de4f9cfe9
Daniel Galán y Martins <git@galan.de>
2016-11-11 16:23:33 +0100

    fix #11 - updating dependencies for jdepend to a fork supporting java 8

--- src/test/java/de/andrena/tools/nopackagecycles/NoPackageCyclesRuleIntegrationTest.java
+++ src/test/java/de/andrena/tools/nopackagecycles/NoPackageCyclesRuleIntegrationTest.java
@@ -1,21 +1,21 @@
 package de.andrena.tools.nopackagecycles;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import de.andrena.tools.nopackagecycles.mock.EnforcerRuleHelperMock;
+import org.apache.commons.io.IOUtils;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.*;
 
-import de.andrena.tools.nopackagecycles.mock.EnforcerRuleHelperMock;
 
 public class NoPackageCyclesRuleIntegrationTest {
+
 	private static final URL FITNESSE_TARGET_FOLDER = getResource("fitnesse-target");
 	private static final URL FITNESSE_EXPECTED_OUTPUT = getResource("fitnesse-expected-output.txt");
 	private static final URL JUNIT_TARGET_FOLDER = getResource("junit-target");
@@ -24,21 +24,25 @@ public class NoPackageCyclesRuleIntegrationTest {
 	private NoPackageCyclesRule rule;
 	private EnforcerRuleHelperMock helper;
 
+
 	@Before
 	public void setUp() throws Exception {
 		rule = new NoPackageCyclesRule();
 		helper = new EnforcerRuleHelperMock();
 	}
 
+
 	@Test
 	public void fitnesseIntegrationTest() throws Exception {
 		assertPackageCycles(FITNESSE_TARGET_FOLDER, FITNESSE_EXPECTED_OUTPUT);
 	}
 
+
 	@Test
 	public void junitIntegrationTest() throws Exception {
 		assertPackageCycles(JUNIT_TARGET_FOLDER, JUNIT_EXPECTED_OUTPUT);
 	}
+
 
 	private void assertPackageCycles(URL targetFolder, URL expectedOutput) throws URISyntaxException, IOException {
 		helper.setTestClassesDir(new File("non-existent"));
@@ -46,15 +50,18 @@ public class NoPackageCyclesRuleIntegrationTest {
 		try {
 			rule.execute(helper);
 			fail("expected EnforcerRuleException");
-		} catch (EnforcerRuleException e) {
+		}
+		catch (EnforcerRuleException e) {
 			// using assertEquals to get a nice comparison editor in eclipse
-			assertEquals(getExpectedOutput(expectedOutput), e.getMessage());
+			assertEquals(getExpectedOutput(expectedOutput), e.getMessage() + "\n");
 		}
 	}
+
 
 	private String getExpectedOutput(URL expectedOutput) throws IOException {
 		return IOUtils.toString(expectedOutput.openStream()).replaceAll("\r", "");
 	}
+
 
 	private static URL getResource(String path) {
 		return Thread.currentThread().getContextClassLoader().getResource(path);
